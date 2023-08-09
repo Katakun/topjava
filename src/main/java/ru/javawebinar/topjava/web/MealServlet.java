b9a0b648739ff9760ea8d61261432a2b11447797
alexandr <k4t4kun@gmail.com>
2023-08-09 18:28:23 +0200

    Lesson 1. Show MealTo in table

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -1,15 +1,31 @@
 package ru.javawebinar.topjava.web;
 
+import ru.javawebinar.topjava.model.MealTo;
+import ru.javawebinar.topjava.util.MealsUtil;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalTime;
+import java.util.List;
 
 public class MealServlet extends HttpServlet {
+    private List<MealTo> mealToList;
+    private int calPerDay = MealsUtil.CALORIES_PER_DAY;
+    private LocalTime startTime = LocalTime.of(7, 0);
+    private LocalTime endTime = LocalTime.of(21, 0);
+
+    public void init() throws ServletException {
+        mealToList = MealsUtil.filteredByStreams(MealsUtil.getMealList(),
+                startTime, endTime, calPerDay);
+    }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.sendRedirect("meals.jsp");
+        request.setAttribute("mealToList", mealToList);
+        request.getRequestDispatcher("meals.jsp").forward(request, response);
+//        response.sendRedirect("meals.jsp");
     }
 }
--- src/main/webapp/meals.jsp
+++ src/main/webapp/meals.jsp
@@ -1,11 +1,31 @@
 <%@ page contentType="text/html;charset=UTF-8" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html lang="ru">
 <head>
     <title>Meals</title>
 </head>
 <body>
-<h3><a href="index.html">Home</a> </h3>
+<h3><a href="index.html">Home</a></h3>
 <hr>
 <h2>Meals</h2>
+<table>
+    <tr>
+        <th>Date</th>
+        <th>Description</th>
+        <th>Calories</th>
+        <th></th>
+        <th></th>
+    </tr>
+    <c:forEach items="${mealToList}" var="mealsTo">
+        <jsp:useBean id="mealsTo" type="ru.javawebinar.topjava.model.MealTo"/>
+        <tr>
+            <td>${mealsTo.dateTime}</td>
+            <td>${mealsTo.description}</td>
+            <td>${mealsTo.calories}</td>
+            <td>Update</td>
+            <td>Delete</td>
+        </tr>
+    </c:forEach>
+</table>
 </body>
 </html>
