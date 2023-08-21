bcb44637734dbb57d4d3750e430395cba53eca12
alexandr <k4t4kun@gmail.com>
2023-08-21 13:02:53 +0200

    L1.Opt. Refactor MealServlet.java

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -27,7 +27,7 @@ public class MealServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws javax.servlet.ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
-        LocalDateTime localDateTime = LocalDateTime.parse(request.getParameter("dateTime"), DATE_TIME_FORMATTER);
+        LocalDateTime localDateTime = LocalDateTime.parse(request.getParameter("dateTime"));
         String description = request.getParameter("description");
         int calories = Integer.parseInt(request.getParameter("calories"));
         String action = request.getParameter("action");
