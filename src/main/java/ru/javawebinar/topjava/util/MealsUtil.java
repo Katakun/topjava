09a83151a131b2eb23896c9ffe594a7162528ad9
alexandr <k4t4kun@gmail.com>
2023-08-24 15:17:31 +0200

    L1.Opt. Refactor MealsUtil.java

--- src/main/java/ru/javawebinar/topjava/util/MealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/MealsUtil.java
@@ -7,7 +7,6 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
